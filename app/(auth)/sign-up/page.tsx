8dc3533474508de2eb055f19b379a43a9268cc0f
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-08-22 23:09:59 +0100

    auth pages UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
+import React from "react";
 
 const SignUp = () => {
   return (
     <div>
-      
+      <section className="flex-center size-full max-sm:px-6">
+        <AuthForm type="sign-up" />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
